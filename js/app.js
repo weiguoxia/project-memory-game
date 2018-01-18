2c6ac3ec5d9b55da3eaa6c614163fedf9376194a
weiguo xia <xiaw@miamioh.edu>
2018-01-18 09:44:53 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,70 @@
 /*
  * Create a list that holds all of your cards
  */
+let cards = ['fa-paper-plane-o', 'fa-diamond','fa-anchor', 'fa-bolt', 'fa-cube',
+             'fa-leaf', 'fa-bicycle','fa-bomb', 'fa-paper-plane-o', 'fa-diamond',
+             'fa-anchor', 'fa-bolt', 'fa-cube','fa-leaf', 'fa-bicycle','fa-bomb',];
 
+let previousCard = null;
+let countMatch = 0;
+let moves = 0;
+let startTime = new Date().getTime();
+function clockString() {
+  var now = new Date().getTime();
+  var distance = now - startTime;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  return days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}
+
+function clock() {
+  // Display the result in the element with id="demo"
+  document.getElementById("clock").innerHTML = clockString();
+}
+
+var x = setInterval(clock, 1000);
+
+function showWinPage(numOfStars) {
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+
+
+  const htmlDiv = "<div class='center-div'> </div>";
+  const htmlClock = '<span>'+ clockString()+'  </span>'
+  const htmlTitle = '<h1> Congratulations! you won in '+ htmlClock +'! </h1>';
+  const htmlContent = "<p>With " + moves + " moves and " + numOfStars + " Stars.<br> Woooooooo! </p>";
+  const htmlCheck = "<div class='fa fa-check-circle-o fa-5x' aria-hidden='true'></div>";
+  const htmlButton = '<button onclick="location.reload()">Play again</button>';
+  document.body.insertAdjacentHTML("afterBegin",htmlDiv);
+  const div = document.querySelector("div");
+  div.insertAdjacentHTML("afterbegin", htmlButton);
+  div.insertAdjacentHTML("afterbegin", htmlContent);
+  div.insertAdjacentHTML("afterbegin", htmlTitle);
+  div.insertAdjacentHTML("afterbegin", htmlCheck);
+}
+
+function restart() {
+  let shuffledCards = shuffle(cards);
+  let ul = document.querySelector(".deck");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
+  shuffledCards.forEach(function(card) {
+    const htmlTextToAdd = '<li class="card"><i class="fa '+ card +' "></i></li>';
+    ul.insertAdjacentHTML('afterbegin', htmlTextToAdd);
+  });
+  countMatch = 0;
+  moves = 0;
+  updateMoves();
+  updateStars();
+  clearInterval(x);
+  startTime = new Date().getTime();
+  x = setInterval(clock, 1000);
+}
 
 /*
  * Display the cards on the page
@@ -12,19 +75,18 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +98,56 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function updateMoves() {
+  document.querySelector(".moves").textContent=moves;
+  updateStars();
+}
+
+function updateStars() {
+  if(moves == 0) {
+    let ul = document.getElementsByClassName("fa-star-o");
+    for (i = 0; i < ul.length; i++) {
+      ul[i].classList.remove("fa-star-o");
+      ul[i].classList.add("fa-star");
+    }
+  }else if(moves === 3) {
+    let ul = document.querySelector(".stars");
+    ul.firstElementChild.firstElementChild.classList.add("fa-star-o");
+    ul.firstElementChild.firstElementChild.classList.remove("fa-star");
+  } else if(moves === 10) {
+    let ul = document.querySelector(".stars");
+    ul.children[1].firstElementChild.classList.remove("fa-star");
+    ul.children[1].firstElementChild.classList.add("fa-star-o");
+  }
+}
+
+document.querySelector(".deck").addEventListener('click', function(event) {
+  let currentCard = event.target;
+  if(currentCard.nodeName === "LI" && currentCard !== previousCard) {
+    moves++;
+    updateMoves();
+    if(previousCard === null) {
+      currentCard.classList.add("show", "open");
+      previousCard = currentCard;
+    } else if(currentCard.querySelector('i').className === previousCard.querySelector('i').className) {
+      currentCard.classList.add("match", "open");
+      previousCard.classList.add("match", "open");
+      if(++countMatch === 8) {
+        const numOfStars = document.getElementsByClassName("fa-star");
+        setTimeout(showWinPage,1000, numOfStars.length);
+      };
+    } else {
+      currentCard.classList.add("open", "show");
+      setTimeout(function() {
+        currentCard.classList.remove("open", "show");
+        previousCard.classList.remove("open", "show");
+        previousCard = null;
+      }, 500);
+    }
+  }
+});
+
+document.querySelector(".restart").addEventListener("click",restart);
+
+restart();
